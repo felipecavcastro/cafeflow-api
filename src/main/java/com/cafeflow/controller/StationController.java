bf03ff03061d980b0c51fba90f80be15b29e3c9b
Felipe <felipecavcastro@gmail.com>
2026-04-24 15:45:57 -0300

    feat: inclusão do método Delete

--- src/main/java/com/cafeflow/controller/StationController.java
+++ src/main/java/com/cafeflow/controller/StationController.java
@@ -21,10 +21,23 @@ public class StationController {
     public List<Station> getAll() {
         return repository.findAll();
     }
+    // Buscar apenas as mesas que estão livres para o nômade sentar agora
+    @GetMapping("/available")
+    public List<Station> getAvailableStations() {
+        return repository.findByIsAvailableTrue();
+    }
+
 
     // Cadastrar uma nova mesa (Ex: "Mesa da Janela com Tomada")
     @PostMapping
     public Station create(@RequestBody Station station) {
         return repository.save(station);
     }
+
+    // Deletar uma mesa pelo ID
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+
 }
--- src/main/java/com/cafeflow/repository/StationRepository.java
+++ src/main/java/com/cafeflow/repository/StationRepository.java
@@ -3,7 +3,11 @@ package com.cafeflow.repository;
 import com.cafeflow.model.Station;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface StationRepository extends JpaRepository<Station, Long> {
+
+    // O Spring entende que deve buscar onde o campo 'isAvailable' é true
+    List<Station> findByIsAvailableTrue();
 }
